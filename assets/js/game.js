1c924190e9f5e8bdfeb2511944711287fe4b1256
gilmored <21026373+gilmored@users.noreply.github.com>
2022-01-16 20:49:15 -0600

    updated playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,8 +65,19 @@ enemy.health = Math.max(0, enemy.health - damage);
     }
   };
 
+    // function to set name
+    var getPlayerName = function() {
+        var name = "";
+      
+        while (name === "" || name === null) {
+          name = prompt("What is your robot's name?");
+        }
+        console.log("Your robot's name is " + name);
+        return name;
+      };
+
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -127,7 +138,7 @@ for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
+ 
       // pick new enemy to fight based on the index of the enemy.name array
       var pickedEnemyObj = enemyInfo[i];
   
